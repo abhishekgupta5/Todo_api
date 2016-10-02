02ae00db8f6892c5db7458f13aaeaf3068a95a4d
abhishekgupta5 <abhishekmodi2011@gmail.com>
2016-10-02 13:33:22 +0530

    PUT and DELETE requests added

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ def index():
     return "Hello,World"
 
 
+#GET requests
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
 def get_tasks():
     return jsonify({'tasks': tasks})
@@ -35,6 +36,7 @@ def get_task(task_id):
         abort(404)
     return jsonify({'tasks': task[0]})
 
+#POST request
 @app.route('/todo/api/v1.0/tasks', methods=['POST'])
 def create_task():
     if not request.json or not 'title' in request.json:
@@ -48,9 +50,39 @@ def create_task():
     tasks.append(task)
     return jsonify({'task': task}), 201
 
+#PUT request
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) is not unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})
+
+#DELETE request
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result': True})
+
+#For returning json in place of HTML, if encounter 404
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
+
 if __name__ == '__main__':
     app.run(debug=True)
 
