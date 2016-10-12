d4794038395e2eed296c9ddcef984b892eb0ea4a
Abhishek Gupta <abhishekmodi2011@gmail.com>
2016-10-13 01:24:51 +0530

    Minor changes- seats functionality

--- app.py
+++ app.py
@@ -3,17 +3,23 @@ from flask import Flask, jsonify, abort, make_response, request, url_for
 
 app = Flask(__name__)
 
-tasks = [
+seats = [
     {
         'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Pasta',
+        'title': u'A1',
+        'seat_type': u'Balcony',
         'done': False
    },
     {
         'id': 2,
-        'title': u'Learn flask',
-        'description': u'Find good flask tutorial on the web',
+        'title': u'K5',
+        'seat_type': u'Platinum',
+        'done': False
+    },
+    {
+        'id':3,
+        'title': u'C3',
+        'seat_type': u'Gold',
         'done': False
     }
 
@@ -25,58 +31,58 @@ def index():
 
 
 #GET requests
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': [ make_public_task(task) for task in tasks] })
+@app.route('/api/v1.0/seats', methods=['GET'])
+def get_seats():
+    return jsonify({'seats': [ make_public_seat(seat) for seat in seats] })
 
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
+@app.route('/api/v1.0/seats/<int:seat_id>', methods=['GET'])
+def get_seat(seat_id):
+    seat = [seat for seat in seats if seat['id'] == seat_id]
+    if len(seat) == 0:
         abort(404)
-    return jsonify({'tasks': task[0]})
+    return jsonify({'seats': seat[0]})
 
 #POST request
-@app.route('/todo/api/v1.0/tasks', methods=['POST'])
-def create_task():
+@app.route('/api/v1.0/seats', methods=['POST'])
+def create_seat():
     if not request.json or not 'title' in request.json:
         abort(400)
-    task = {
-        'id': tasks[-1]['id'] + 1,
+    seat = {
+        'id': seats[-1]['id'] + 1,
         'title': request.json['title'],
-        'description': request.json.get('description',""),
+        'seat_type': request.json.get('seat_type',""),
         'done': False
         }
-    tasks.append(task)
-    return jsonify({'task': task}), 201
+    seats.append(seat)
+    return jsonify({'seat': seat}), 201
 
 #PUT request
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
-def update_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
+@app.route('/api/v1.0/seats/<int:seat_id>', methods=['PUT'])
+def update_seat(seat_id):
+    seat = [seat for seat in seats if seat['id'] == seat_id]
+    if len(seat) == 0:
         abort(404)
     if not request.json:
         abort(400)
 #It's to note that since we are using python3, we are using 'str' here, in case of python2, 'str' needs to replace with 'unicode'
     if 'title' in request.json and type(request.json['title']) is not str:
         abort(400)
-    if 'description' in request.json and type(request.json['description']) is not str:
+    if 'seat_type' in request.json and type(request.json['seat_type']) is not str:
         abort(400)
     if 'done' in request.json and type(request.json['done']) is not bool:
         abort(400)
-    task[0]['title'] = request.json.get('title', task[0]['title'])
-    task[0]['description'] = request.json.get('description', task[0]['description'])
-    task[0]['done'] = request.json.get('done', task[0]['done'])
-    return jsonify({'task': task[0]})
+    seat[0]['title'] = request.json.get('title', seat[0]['title'])
+    seat[0]['seat_type'] = request.json.get('seat_type', seat[0]['seat_type'])
+    seat[0]['done'] = request.json.get('done', seat[0]['done'])
+    return jsonify({'seat': seat[0]})
 
 #DELETE request
-@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
-def delete_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
+@app.route('/api/v1.0/seats/<int:seat_id>', methods=['DELETE'])
+def delete_seat(seat_id):
+    seat = [seat for seat in seats if seat['id'] == seat_id]
+    if len(seat) == 0:
         abort(404)
-    tasks.remove(task[0])
+    seats.remove(seat[0])
     return jsonify({'result': True})
 
 #For returning json in place of HTML, if encounter 404
@@ -84,14 +90,14 @@ def delete_task(task_id):
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
-def make_public_task(task):
-    new_task = {}
-    for field in task:
+def make_public_seat(seat):
+    new_seat = {}
+    for field in seat:
         if field == 'id':
-            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
+            new_seat['uri'] = url_for('get_seat', seat_id=seat['id'], _external=True)
         else:
-            new_task[field] = task[field]
-    return new_task
+            new_seat[field] = seat[field]
+    return new_seat
 
 if __name__ == '__main__':
     app.run(debug=True)
